84caf9a88aba1d9e99a317a7afad0b84f60b61f4
Nick <nwestallen@gmail.com>
2021-04-02 09:34:04 -0400

    reverting authentication changes

--- api/middleware/restrict.js
+++ api/middleware/restrict.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   if (!token) {
     res.status(401).json({ message: 'token required' });
   } else {
-    jwt.verify(token.replace(/^Bearer\s+/, ""), JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
         res.status(401).json({ message: 'token invalid'});
       } else {
