126ca8ed4ecdf2003a5a0f66e01ab69140a5ef29
Nick <nwestallen@gmail.com>
2021-04-02 09:27:33 -0400

    try again

--- api/middleware/restrict.js
+++ api/middleware/restrict.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../auth/secrets');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.replace(/^Bearer\s+/, "");
+  let token = req.headers.authorization;
 
   if (!token) {
     res.status(401).json({ message: 'token required' });
   } else {
-    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    jwt.verify(token.replace(/^Bearer\s+/, ""), JWT_SECRET, (err, decoded) => {
       if (err) {
         res.status(401).json({ message: 'token invalid'});
       } else {
